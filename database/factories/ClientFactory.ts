4dc6e9dfcaad86bafd040f4e4927cb16a3c9f495
Amel Alexandre <alexandre@Mini-de-Amel.home>
2023-10-18 13:36:17 +0200

    test for deploy

--- database/factories/ClientFactory.ts
+++
@@ -1,8 +0,0 @@
-import client from 'App/Models/client'
-import Factory from '@ioc:Adonis/Lucid/Factory'
-
-export default Factory.define(client, ({ faker }) => {
-  return {
-    //
-  }
-}).build()
