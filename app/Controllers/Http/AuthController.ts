030b27193ca5b06df0a31408abb662f3dae9a22a
Alexandre <al.piresdealmeida@gmail.com>
2023-10-19 16:52:01 +0200

    add mail installation, modif video with voice

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,4 +1,3 @@
-import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Client from 'App/Models/Client'
 import User from 'App/Models/User'
