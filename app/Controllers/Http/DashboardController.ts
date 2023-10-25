b5d8bec385526c6f6f0c8cae24da8eee9af478b0
Alexandre <al.piresdealmeida@gmail.com>
2023-10-25 12:15:31 +0200

    fix: error with values coma

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -27,38 +27,57 @@ export default class DashboardController {
     const user = await auth.authenticate()
     const clients = await Client.query().where('user_id', user.id)
 
-    const accumulatedData = new Map<string, number>()
-    let totalSomme = 0
+    const monthlyData = {}
 
-    clients.forEach((client) => {
-      const date = new Date(client.month)
-      const monthYear = `${date.getMonth() + 1}/${date.getFullYear()}`
-      const key = monthYear
+    for (const client of clients) {
+      const monthKey = client.month.toString().slice(0, 7).split('-').reverse().join('-')
+
+      if (!monthlyData[monthKey]) {
+        monthlyData[monthKey] = {
+          totalSomme: 0,
+          clients: [],
+        }
+      }
 
       const somme = client.somme
 
-      if (accumulatedData.has(key)) {
-        accumulatedData.set(key, accumulatedData.get(key)! + somme)
-      } else {
-        accumulatedData.set(key, (accumulatedData.get(key) || 0) + somme)
-      }
-
-      totalSomme += somme
-    })
-
-    // Créer une fonction de comparaison pour trier les dates
-    function compareDates(a: string, b: string) {
-      const dateA = new Date(a.split('/').reverse().join('/'))
-      const dateB = new Date(b.split('/').reverse().join('/'))
-      return dateA.getTime() - dateB.getTime()
+      monthlyData[monthKey].totalSomme += somme
+      monthlyData[monthKey].clients.push({
+        name: client.name,
+        somme: somme,
+      })
     }
 
-    // Convertir les clés (les mois) en objets Date et trier
-    const sortedKeys = Array.from(accumulatedData.keys()).sort(compareDates)
+    const dateByMonth = {}
 
-    // Générer les dataValues en fonction des labels triés
-    const sortedDataValues = sortedKeys.map((label) => accumulatedData.get(label)!)
-    return response.json({ labels: sortedKeys, dataValues: sortedDataValues, totalSomme })
+    for (const monthKey in monthlyData) {
+      if (Object.prototype.hasOwnProperty.call(monthlyData, monthKey)) {
+        const clients = monthlyData[monthKey].clients
+        let totalSomme = 0
+
+        for (const client of clients) {
+          const somme = parseFloat(client.somme)
+          totalSomme += somme
+        }
+
+        if (!dateByMonth[monthKey]) {
+          dateByMonth[monthKey] = []
+        }
+
+        dateByMonth[monthKey].push({
+          clients: clients,
+          totalSomme: totalSomme.toFixed(2),
+        })
+      }
+    }
+    const transformedData = dateByMonth
+
+    const labels = Object.keys(transformedData)
+    const dataValues = Object.keys(transformedData).map(
+      (monthKey) => transformedData[monthKey][0].totalSomme
+    )
+    console.log(labels, dataValues)
+    return response.json({ labels, dataValues })
   }
 
   public showSettings({ view }: HttpContextContract) {
@@ -94,12 +113,14 @@ export default class DashboardController {
 
     clients.forEach((client) => {
       const key = client.name
+      const somme = parseFloat(client.somme.toString())
 
       if (clientData.has(key)) {
-        clientData.get(key)!.somme += client.somme
+        const currentSomme = clientData.get(key)!.somme || 0
+        clientData.get(key)!.somme = currentSomme + somme
         clientData.get(key)!.dates.push(client.month)
       } else {
-        clientData.set(key, { somme: client.somme, dates: [client.month] })
+        clientData.set(key, { somme, dates: [client.month] })
       }
     })
 
@@ -163,7 +184,6 @@ export default class DashboardController {
         })
       }
     }
-    console.log(dateByMonth) // OK JUSQUICI
 
     const transformedData = {}
 
@@ -179,7 +199,6 @@ export default class DashboardController {
         }
       }
     }
-    console.log(transformedData)
     return view.render('showMonth', { transformedData })
   }
 }
